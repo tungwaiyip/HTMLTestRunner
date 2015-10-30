13008c8164a7afe276d7625eb27975649dfd6eeb
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2015-10-30 01:56:25 -0200

    Moves the how to use to the README file

--- HTMLTestRunner.py
+++ HTMLTestRunner.py
@@ -1,39 +1,3 @@
-"""
-A TestRunner for use with the Python unit testing framework. It
-generates a HTML report to show the result at a glance.
-
-The simplest way to use this is to invoke its main method. E.g.
-
-    import unittest
-    import HTMLTestRunner
-
-    ... define your tests ...
-
-    if __name__ == '__main__':
-        HTMLTestRunner.main()
-
-
-For more customization options, instantiates a HTMLTestRunner object.
-HTMLTestRunner is a counterpart to unittest's TextTestRunner. E.g.
-
-    # output to a file
-    fp = file('my_report.html', 'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(
-                stream=fp,
-                title='My unit test',
-                description='This demonstrates the report output by HTMLTestRunner.'
-                )
-
-    # Use an external stylesheet.
-    # See the Template_mixin class for more customizable options
-    runner.STYLESHEET_TMPL = '<link rel="stylesheet" href="my_stylesheet.css" type="text/css">'
-
-    # run the test
-    runner.run(my_test_suite)
-"""
-
-# URL: http://tungwaiyip.info/software/HTMLTestRunner.html
-
 __author__ = "Wai Yip Tung"
 __version__ = "0.8.3"
 
