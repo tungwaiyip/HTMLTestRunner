97085a4c8004cf8afbf09df6d82a7f264f657f8a
Dhruv Paranjape <lord.dhruv@gmail.com>
2016-08-20 09:37:54 +0530

    minor import cleanup and add skip testcase
    add skip testcase and cleanup the imports

--- test_HTMLTestRunner.py
+++ test_HTMLTestRunner.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
 
-import io as StringIO
-import sys
-import unittest
-import HTMLTestRunner
+from io import StringIO
+from unittest import TestSuite, TestLoader, skip, TextTestRunner, TestCase
+from HTMLTestRunner import HTMLTestRunner
 
 import tests
 from tests.SampleTestPass import SampleTestPass
 from tests.SampleTestFail import SampleTestFail
 from tests.SampleTestBasic import SampleTestBasic
 
-class TestHTMLTestRunner(unittest.TestCase):
+class TestHTMLTestRunner(TestCase):
     
     def setUp(self):
    
-        self.suite = unittest.TestSuite()
-        self.loader = unittest.TestLoader()
+        self.suite = TestSuite()
+        self.loader = TestLoader()
 
         self.suite.addTests(self.loader.loadTestsFromModule(tests.SampleTestPass))
         self.suite.addTests(self.loader.loadTestsFromModule(tests.SampleTestFail))
         self.suite.addTests(self.loader.loadTestsFromModule(tests.SampleTestBasic))
 
-        self.results_output_buffer = StringIO.StringIO()
-        HTMLTestRunner.HTMLTestRunner(stream=self.results_output_buffer).run(self.suite)
+        self.results_output_buffer = StringIO()
+        HTMLTestRunner(stream=self.results_output_buffer).run(self.suite)
         self.byte_output = self.results_output_buffer.getvalue()
 
     def test_SampleTestPass(self):
@@ -30,7 +29,7 @@ class TestHTMLTestRunner(unittest.TestCase):
         output2="".join(SampleTestPass.EXPECTED_RESULT.split())
         self.assertGreater(output1.find(output2),0)
     
-    @unittest.skip("Test Skipping")
+    @skip("Test Skipping")
     def test_SampleTestSkip(self):
         self.fail("This error should never be displayed")
         
@@ -46,8 +45,8 @@ class TestHTMLTestRunner(unittest.TestCase):
 
 
 def main():
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestHTMLTestRunner)
-    unittest.TextTestRunner().run(suite)
+    suite = TestLoader().loadTestsFromTestCase(TestHTMLTestRunner)
+    TextTestRunner().run(suite)
 
 if __name__ == "__main__":
     main()
